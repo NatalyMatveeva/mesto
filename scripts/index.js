0485a28ce74b1112c7c7c45377947e276168a649
Natalia Matveeva <matveevanatig89@yandex.ru>
2022-03-24 13:23:29 +0300

    Проектная работа 4 Отпрвка №2

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
 const popupElement = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__form')
+const nameInput = formElement.querySelector('.popup__name');
+const profInput = formElement.querySelector('.popup__prof');
+const nameText = document.querySelector('.profile__name');
+const profText = document.querySelector('.profile__prof')
 
 function openPopup() {
   popupElement.classList.add('popup_opened');
+  nameInput.value = nameText.textContent;
+  profInput.value = profText.textContent;
 }
 
 function closePopup() {
@@ -13,20 +20,11 @@ function closePopup() {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
-
-
-let formElement = document.querySelector('.popup__form')
-let nameInput = formElement.querySelector('.popup__name');
-let profInput = formElement.querySelector('.popup__prof');
-
-let nameText = document.querySelector('.profile__name');
-let profText = document.querySelector('.profile__prof')
-
 function formSubmitHandler (evt) {
 evt.preventDefault();
 nameText.textContent = nameInput.value;
 profText.textContent = profInput.value;
-popupElement.classList.remove('popup_opened');
+closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
